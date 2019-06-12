cc3f4b827f8c8fb9385d6acbfd53532fb172ec01
Srikanth Davu <srikanth.davu@nasa.gov>
2019-06-12 14:48:27 -0400

    CMR-5631 Fixing assets path in env deployments

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -35,7 +35,6 @@ Rails.application.configure do
   # yet still be able to expire them through the digest params.
   config.assets.digest = true
   config.assets.precompile =  %w[*.js *.css *.css.erb *.png *.jpg *.jpeg *.gif]
-  #config.assets.prefix = '/csw/assets'
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
