e298050229069d2a511b6d3f5a36c97bc016c733
Daniel Zamora <daniel.p.zamora@nasa.gov>
2020-03-12 13:35:54 -0400

    CMR-6267 Fixes deployments

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,7 @@ require 'action_controller/railtie'
 require 'action_mailer/railtie'
 require 'action_view/railtie'
 require 'rails/test_unit/railtie'
+require 'sprockets/railtie'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
